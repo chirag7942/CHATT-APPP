771241379583c420973df8078f740e7a9db769b6
Chirag Singh <cchiragg.27@gmail.com>
2025-05-24 18:33:06 +0530

    Update useAuthStore.js

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -5,7 +5,7 @@ import { axiosInstance } from "../lib/axios.js";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-const BASE_URL = "http://localhost:5001"  ;
+const BASE_URL = "https://chatt-appp.onrender.com"  ;
 
 export const useAuthStore = create((set, get) => ({
 
