5c99951e0b1ee35563d0414d579052398c292011
Chirag Singh <cchiragg.27@gmail.com>
2025-05-24 18:33:31 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL:  "http://localhost:5001/api" ,//it means every time we'll make request to this api
+  baseURL:  "https://chatt-appp.onrender.com//api" ,//it means every time we'll make request to this api
   withCredentials: true, //this withcredntials field will enable client to send cookies for login or signup.
 
 });
